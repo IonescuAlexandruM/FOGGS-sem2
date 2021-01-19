86c17f49adde11101005caf1b840207b629b911f
IonescuAlexandruM <i010391k@student.staffs.ac.uk>
2021-01-19 17:54:54 +0000

    helloGL

---
+++ HelloGL/HelloGL/GlutCallbacks.cpp
@@ -0,0 +1,26 @@
+#include "GLUTCallbacks.h"
+#include "HelloGL.h"
+
+
+namespace GLUTCallbacks
+{
+	namespace
+	{ 
+		//initialise to a null pointer before we do anything
+		HelloGL* helloGL = nullptr;
+	}
+
+	void Init(HelloGL*gl)
+	{
+		helloGL = gl;
+	}
+
+	void Display()
+	{
+		if (helloGL != nullptr)
+		{
+			helloGL->Display();
+		}
+	}
+}
+
---
+++ HelloGL/HelloGL/Source.cpp
@@ -0,0 +1,10 @@
+#include "HelloGL.h"
+
+
+int main(int argc,char* argv[])
+{
+	HelloGL* game = new HelloGL(argc, argv);
+	return 0;
+}
+
+
